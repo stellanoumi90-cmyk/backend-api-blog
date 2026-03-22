1625fda3b335d07ee1ab216a45684429b525ed9a
stellanoumi90-cmyk <stellanoumi90@gmail.com>
2026-03-22 22:39:26 +0100

    Add CRUD routes for articles

--- routes/articles.js
+++ routes/articles.js
@@ -1,1 +1,18 @@
+const express = require('express');
+const router = express.Router();
+const articleController = require('../controllers/articleController');
+const { articleValidationRules, articleUpdateValidationRules } = require('../middlewares/validators');
 
+router.post('/articles', articleValidationRules(), articleController.createArticle);
+
+router.get('/articles', articleController.getAllArticles);
+
+router.get('/articles/search', articleController.searchArticles);
+
+router.get('/articles/:id', articleController.getArticleById);
+
+router.put('/articles/:id', articleUpdateValidationRules(), articleController.updateArticle);
+
+router.delete('/articles/:id', articleController.deleteArticle);
+
+module.exports = router;
